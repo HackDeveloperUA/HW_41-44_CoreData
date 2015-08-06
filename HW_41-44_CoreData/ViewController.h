60e554c3025470be23a66b4236be5c5c614b1b8d
MD <md@MacBook-Air-MD.Dlink>
2015-08-06 12:54:49 +0300

    Initial Commit

---
+++ HW_41-44_CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW_41-44_CoreData
+//
+//  Created by MD on 06.08.15.
+//  Copyright (c) 2015 MD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
