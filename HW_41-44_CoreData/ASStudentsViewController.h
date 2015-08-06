a1e22b857bcefc94486b47037a09e1d8d971a8c7
MD <md@MacBook-Air-MD.Dlink>
2015-08-06 12:57:49 +0300

    Second Commit

--- HW_41-44_CoreData/ASStudentsViewController.h
+++ HW_41-44_CoreData/ASStudentsViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ASStudentsViewController.h
 //  HW_41-44_CoreData
 //
 //  Created by MD on 06.08.15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ASStudentsViewController : UITableViewController
 
 @end
-
